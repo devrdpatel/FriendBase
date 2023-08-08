1f295921d91ca8ebe77adb716df5f6abadda0ea8
devrdpatel <106772575+devrdpatel@users.noreply.github.com>
2023-08-08 18:50:45 -0500

    Added some documentation for the FilteredList View

--- FriendBase/Views/ViewComponents/FilteredList.swift
+++ FriendBase/Views/ViewComponents/FilteredList.swift
@@ -8,13 +8,23 @@
 import CoreData
 import SwiftUI
 
+/// This is a generic View that creates a List after making a FetchRequest for the given type, which will
+/// filter the data based on the given parameters for PredicateKey, sortDescriptors, filterKey and filterValue.
+/// Currently, the list requires a predicateKey but if an empty string is given as the filterValue, no predicate
+/// is actually in filtering the list of objects.
+///
+/// More predicate key options will be added as additional sort features are implemented.
+///
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     
+    // These are used to filter the Core Data stored entities
     enum PredicateKeys: String {
         case contains = "CONTAINS[c]"
     }
     
     @FetchRequest var fetchedResults: FetchedResults<T>
+    
+    // This is the content for what each list row should contain
     let content: (T) -> Content
     
     // Creates the FetchRequest and stores the content closure for later use
